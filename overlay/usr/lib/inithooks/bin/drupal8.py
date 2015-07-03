35b8af1447c7a59dc32159ed907181ad371d74aa
Jeremy Davis <jeremy@turnkeylinux.org>
2015-07-03 01:12:45 +0000

    moved and modified inithooks

--- overlay/usr/lib/inithooks/bin/drupal8.py
+++ overlay/usr/lib/inithooks/bin/drupal8.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-"""Set Drupal7 admin password and email
+"""Set Drupal8 admin password and email
 
 Option:
     --pass=     unless provided, will ask interactively
@@ -22,6 +22,7 @@ def usage(s=None):
     print >> sys.stderr, __doc__
     sys.exit(1)
 
+
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
@@ -42,24 +43,27 @@ def main():
     if not password:
         d = Dialog('TurnKey Linux - First boot configuration')
         password = d.get_password(
-            "Drupal7 Password",
-            "Enter new password for the Drupal7 'admin' account.")
+            "Drupal8 Password",
+            "Enter new password for the Drupal8 'admin' account.")
 
     if not email:
         if 'd' not in locals():
             d = Dialog('TurnKey Linux - First boot configuration')
 
         email = d.get_email(
-            "Drupal7 Email",
-            "Enter email address for the Drupal7 'admin' account.",
+            "Drupal8 Email",
+            "Enter email address for the Drupal8 'admin' account.",
+
             "admin@example.com")
 
     m = MySQL()
-    m.execute('UPDATE drupal7.users SET mail=\"%s\" WHERE name=\"admin\";' % email)
-    m.execute('UPDATE drupal7.users SET init=\"%s\" WHERE name=\"admin\";' % email)
-    system("/usr/bin/drush variable-set site_mail %s" % email)
-    system("/usr/bin/drush variable-set update_notify_emails %s" % email)
-    system("/usr/bin/drush user-password admin --password='%s'" % password)
+    m.execute('UPDATE drupal8.users_field_data SET mail=\"%s\" WHERE name=\"admin\";' % email)
+    m.execute('UPDATE drupal8.users_field_data SET init=\"%s\" WHERE name=\"admin\";' % email)
+    system("/usr/local/bin/drush -y config-set contact.form.feedback recipients %s" % email)
+    system("/usr/local/bin/drush -y config-set update.settings notification.emails.0 %s" % email)
+    system("/usr/local/bin/drush -y config-set system.site mail %s" % email)
+    system("/usr/local/bin/drush cache-rebuild")
+    system("/usr/local/bin/drush user-password admin --password='%s'" % password)
 
 if __name__ == "__main__":
     main()
