b1844e5c3450c5ed93890900f5cf35ea1218137c
Jeremy Davis <jeremy@turnkeylinux.org>
2018-10-18 13:43:36 +1100

    tweak inithook - drush was failing

--- overlay/usr/lib/inithooks/bin/drupalconf.sh
+++ overlay/usr/lib/inithooks/bin/drupalconf.sh
@@ -12,9 +12,10 @@ done
 
 sed -i '/\/\/ For UNIX support/a return static::canonicalize("/root");' /var/www/drupal8/vendor/webmozart/path-util/src/Path.php
 
-/var/www/drupal8/vendor/bin/drush -y config-set contact.form.feedback recipients $EMAIL &> /dev/null
-/var/www/drupal8/vendor/bin/drush -y config-set update.settings notification.emails.0 $EMAIL &> /dev/null
-/var/www/drupal8/vendor/bin/drush -y config-set system.site mail $EMAIL &> /dev/null
-/var/www/drupal8/vendor/bin/drush user-password admin $PASSWORD &> /dev/null
+cd /var/www/drupal8
+vendor/bin/drush -y config-set contact.form.feedback recipients $EMAIL &> /dev/null
+vendor/bin/drush -y config-set update.settings notification.emails.0 $EMAIL &> /dev/null
+vendor/bin/drush -y config-set system.site mail $EMAIL &> /dev/null
+vendor/bin/drush user-password admin $PASSWORD &> /dev/null
 sed -i "/^\$settings\['trusted_host_patterns'\]/{n;s|.*|     '^$DOMAIN$',|}" /var/www/drupal8/web/sites/default/settings.php
-/var/www/drupal8/vendor/bin/drush cache-rebuild &> /dev/null
+vendor/bin/drush cache-rebuild &> /dev/null
